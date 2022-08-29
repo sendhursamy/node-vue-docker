3c240c63ad5e59184405f1fd9bd36a8a2c02e9f2
sendhur <sendhursamy.a@thehindu.co.in>
2022-08-29 17:33:21 +0530

    change password functionality done.

--- frontend/agent-care/src/views/ChangepasswordView.vue
+++ frontend/agent-care/src/views/ChangepasswordView.vue
@@ -13,29 +13,41 @@
             </div>
             <div class="card-body p-3 pb-0">
               <div class="row justify-content-center">
-      <div class="col-6">
+      <div class="col-12 col-lg-6">
         <div class="card card-default">
-          
+            <div class="error" v-if="itsInvalid && showInvalidText.trim()!=''">{{showInvalidText}}</div>
           <div class="card-body">
-            <!-- <ul v-if="errors">
-              <li v-for="error in errors" v-bind:key="error">{{ msg }}</li>
-            </ul> -->
-            <form autocomplete="off" @submit.prevent="changePassword" method="post">
-              <div class="form-group">
-                  <label for="email">Current Password</label>
-                  <input type="password" id="password" class="form-control" placeholder="" v-model="password_old" required>
-              </div>
-              <div class="form-group">
-                  <label for="email">Password</label>
-                  <input type="password" id="password" class="form-control" placeholder="" v-model="password" required>
-              </div>
-              <div class="form-group">
-                  <label for="email">Confirm Password</label>
-                  <input type="password" id="password_confirmation" class="form-control" placeholder="" v-model="password_confirmation" required>
-              </div>
-              <button type="submit" class="btn btn-primary" disabled>Update</button>
-            </form>
-          </div>
+              
+              <form role="form" class="text-start" @submit.prevent="changePassword">
+                
+                <div class="input-group input-group-outline my-3">
+                  <label class="form-label">Current Password</label>
+                  <input type="password" @focus="submitted = false" class="form-control"
+                    v-model="password_old" :class="{ 'is-invalid': submitted && !password_old }">
+                  <div v-show="submitted && !password_old" class="invalid-feedback">password old is required</div>
+                </div>
+                <div class="input-group input-group-outline mb-3">
+                  <label class="form-label">New Password</label>
+                  <input type="password" @focus="submitted = false" class="form-control"
+                    v-model="password" :class="{ 'is-invalid': submitted && !password }">
+                  <div v-show="submitted && !password" class="invalid-feedback">password is required</div> 
+                </div>
+                <div class="input-group input-group-outline mb-3">
+                  <label class="form-label">Confirm Password</label>
+                  <input type="password" @focus="submitted = false" class="form-control"
+                    v-model="password_confirm" :class="{ 'is-invalid': submitted && !password_confirm }">
+                  <div v-show="submitted && !password_confirm" class="invalid-feedback">confirm Password is required</div> 
+                </div>
+                <div class="text-center">
+                  <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Update</button>
+                </div>
+                <!-- <p class="mt-4 text-sm text-center">
+                    Don't have an account?
+                    <a href="../pages/sign-up.html" class="text-primary text-gradient font-weight-bold">Sign up</a>
+                  </p> -->
+              </form>
+            </div>
+          
         </div>
       </div>
     </div>
@@ -49,16 +61,25 @@
 
 import MenuWidget from '@/components/MenuView.vue'
 import NavBarwidget from '@/components/NavBarView.vue'
-
+import axios from 'axios'
 export default {
+  data() {
+    return{
+      password_old: '',
+      password: '',
+      password_confirm: '',
+      submitted: false,
+      itsInvalid: false,
+      showInvalidText:'',
+      token : localStorage.getItem("token")
+    }
+  },
   components: {
     MenuWidget,
     NavBarwidget
   },
     methods: {
-      changePassword(){
-        console.warn('enter');
-      },
+      
     toggleNav() {
      let body = document.getElementsByTagName('body')[0];
     let className = 'g-sidenav-pinned';
@@ -91,10 +112,80 @@ export default {
     sidenav.classList.remove('bg-transparent');
 
   }
-  }
+  },
+  focusInput: function () {
+      var inputs = document.querySelectorAll('input');
+      
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('focus', function () {
+          this.parentElement.classList.add('is-focused');
+        }, false);
+
+        inputs[i].onkeyup = function () {          
+          if (this.value != "") {
+            this.parentElement.classList.add('is-filled');
+          } else {
+            this.parentElement.classList.remove('is-filled');
+          }
+        };
+
+        inputs[i].addEventListener('focusout', function () {
+          if (this.value != "") {
+            this.parentElement.classList.add('is-filled');
+          }
+          this.parentElement.classList.remove('is-focused');
+        }, false);
+      }
+    },
+    async changePassword() {
+       this.submitted = true;
+       if(this.password !='' && this.password_confirm !='' && this.password_old !=''){
+       if(this.password == this.password_confirm){
+          const _URL = process.env.VUE_APP_END_POINT;
+          const data = JSON.stringify({ oldPassword: `${this.password_old}`, newPassword: `${this.password}`, confirmPassword: `${this.password_confirm}` })
+
+          var config = {
+          method: 'post',
+          url: _URL +'changepassword?api_token='+this.token,
+          headers: {
+          'Content-Type': 'application/json'
+          },
+          data: data
+          };
+          const vm = this;
+          await axios(config).then(function (response) {         
+            if(response.data.code == 200){ 
+                localStorage.removeItem("token")
+                vm.$router.push('/')        
+            }else{
+              vm.itsInvalid = true,
+              vm.showInvalidText=response.data.message
+              }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+          
+       }else{
+         this.itsInvalid = true,
+         this.showInvalidText="Your password and confirmation password do not match."
+       }
+       }
+        
+     }
 },
   mounted(){
+    if(!this.token){
+      this.$router.push({name:"home"})
+    }
     this.removenav();
+    this.focusInput();
   }
 }
 </script>
+<style>
+.error{
+text-align: center;
+color: red;
+}
+</style>
